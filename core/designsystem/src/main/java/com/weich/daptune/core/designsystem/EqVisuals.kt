1ee9d208423bad5fab2136ba4a9fb2b731501ce8
User-1145141 <garychengdong@gmail.com>
2026-09-07 15:29:45 +0800

    Update EqVisuals.kt
    微调坐标系

--- core/designsystem/src/main/java/com/weich/daptune/core/designsystem/EqVisuals.kt
+++ core/designsystem/src/main/java/com/weich/daptune/core/designsystem/EqVisuals.kt
@@ -145,9 +145,8 @@ fun EqCurveOverview(
                 textLayoutResult = labelLayout,
                 topLeft = Offset(
                     x = 3.dp.toPx(),
-                    y = (y - labelLayout.size.height / 2f +
-                        if (gainQ4 == axis.minimumQ4) 2.dp.toPx() else 0f)
-                        .coerceIn(0f, plotEndY + 2.dp.toPx() - labelLayout.size.height),
+                    y = (y - labelLayout.size.height / 2f
+                        .coerceIn(0f, plotEndY - labelLayout.size.height),
                 ),
             )
         }
